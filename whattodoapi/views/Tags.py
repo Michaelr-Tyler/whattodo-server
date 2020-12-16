6b08e033f237a7325b01e5137df0d0eb81a594cf
Michael-Tyler <miketylerabc@gmail.com>
2020-12-16 12:52:06 -0600

    added creation for tags

--- whattodoapi/views/Tags.py
+++ whattodoapi/views/Tags.py
@@ -2,17 +2,36 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status, serializers
-from django.http.response import HttpResponseServerError
+from django.core.exceptions import ValidationError
+
 from whattodoapi.models import Tags
 
 class TagViewSet(ViewSet):
     """Tag view set"""
 
     def list(self,request):
-        """GET all tag objecta"""
+        """GET all tag object"""
         Tag = Tags.objects.all()
         serialized_Tag = TagSerializer(Tag, many=True)
         return Response(serialized_Tag.data, status=status.HTTP_200_OK)
+
+    def create(self, request):
+        """ """
+        tag = Tags()
+        tag.label = request.data["label"]
+        
+        try:
+            tag.save()
+            serializer = TagSerializer(tag, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def update(self, request, pk=None):
+        """"""
+
+    def destroy(self, request, pk=None):
+        """"""
 
 class TagSerializer(serializers.ModelSerializer):
     """JSON serializer for Tag"""
