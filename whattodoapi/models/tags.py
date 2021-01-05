57a7534a8bef22c88b6f01a521e4d6c3d3c9137d
Michael-Tyler <miketylerabc@gmail.com>
2021-01-04 20:05:59 -0600

    now tags are only created per user

--- whattodoapi/models/tags.py
+++ whattodoapi/models/tags.py
@@ -1,6 +1,9 @@
 """Tag model Module"""
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Tags(models.Model):
     """Tag database model"""
     label = models.CharField(max_length=25)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
